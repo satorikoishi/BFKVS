7b56b502b6857b4fe928cf9738a7329b071d0a2f
Wei Jin <410453494@qq.com>
2022-01-10 10:57:12 +0800

    updated script, create 3 links between nodes

--- BF.py
+++ BF.py
@@ -2,6 +2,7 @@
 
 Instructions:
 Param n: # of nodes
+Param l: # of links between nodes
 """
 
 import geni.portal as portal
@@ -12,30 +13,50 @@ request = portal.context.makeRequestRSpec()
 
 # Describe the parameter(s) this profile script can accept.
 portal.context.defineParameter( "n", "Number of nodes", portal.ParameterType.INTEGER, 2)
+portal.context.defineParameter( "l", "Number of links between nodes", portal.ParameterType.INTEGER, 3)
 
 # Retrieve the values the user specifies during instantiation.
 params = portal.context.bindParameters()
 
 # Check parameter validity.
 if params.n < 1 or params.n > 8:
-    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 8 VMs.", ["n"] ) )
+    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 8 nodes.", ["n"] ) )
+if params.l < 1 or params.l > 4:
+    portal.context.reportError( portal.ParameterError( "You must choose at least 1 and no more than 4 links.", ["l"] ) )
 
 # Abort execution if there are any errors, and report them.
 portal.context.verifyParameters()
 
 nodes = []
+interfaces = []
 # Create n raw "PC" nodes
 for i in range(0, params.n):
     nodes.append(request.RawPC("node" + str(i)))
     nodes[i].hardware_type = "r7525"
     nodes[i].disk_image = "urn:publicid:IDN+clemson.cloudlab.us+image+bfkvs-PG0:BF"
-    
-# links = []
-# for i in range(0, params.n):
-#     links.append(request.Link(members = [x for x in nodes]))
+    node_if = []
+    for j in range(0, params.n):
+        if j == i:
+            if_dup = None
+        else:
+            if_dup = []
+            for m in range(0, params.l):
+                if_dup.append(nodes[i].addInterface('node' + str(i) + 'interface' + str(j) + 'dup' + str(m)))
+        node_if.append(if_dup)
+    interfaces.append(node_if)
 
 # Create link between them
-link = request.Link(members = [x for x in nodes])
+links = []
+link_idx = 0
+for i in range(0, params.n):
+    for j in range(i + 1, params.n):
+        for m in range(0, params.l):
+            link = request.Link('link' + str(link_idx))
+            link_idx = link_idx + 1
+            
+            link.addInterface(interfaces[i][j][m])
+            link.addInterface(interfaces[j][i][m])
+            links.append(link)
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
